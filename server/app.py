156557fca56d6ccac8b7b48ba2ded7f8d99999e3
Shaylan Dias <shaylandias@berkeley.edu>
2020-08-04 10:21:12 -0700

    prepped for deploy w/ correct directories

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, send_file
 import os
 
 app = Flask(__name__, static_folder='../client/build/static')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    print('index endpoint')
+    return send_file('../client/build/index.html')
 
 if __name__ == '__main__':
     app.run(port=(os.getenv('PORT') if os.getenv('PORT') else 8000), debug=False)

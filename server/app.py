2f3f2247db898715daa921ec8fe140fb418809dd
Shaylan Dias <shaylandias@berkeley.edu>
2020-08-04 11:28:38 -0700

    updated w/ static url

--- server/app.py
+++ server/app.py
@@ -1,12 +1,11 @@
-from flask import Flask, send_file
+from flask import Flask 
 import os
 
-app = Flask(__name__, static_folder='../client/build/static')
+app = Flask(__name__, static_folder='../client/build/', static_url_path='/')
 
 @app.route('/')
 def index():
-    print('index endpoint')
-    return send_file('../client/build/index.html')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(port=(os.getenv('PORT') if os.getenv('PORT') else 8000), debug=False)
